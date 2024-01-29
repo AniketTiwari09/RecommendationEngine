ad4ddb290506ba0c1a178e4d70a1e2fcf0ac4931
AniketTiwari09 <atiwari7@uncc.edu>
2024-01-28 22:20:25 -0500

    Updated

--- engines.py
+++ engines.py
@@ -59,5 +59,27 @@ class ContentEngine(object):
 
             flattened = sum(similar_items[1:], ())
             self._r.zadd(self.SIMKEY % row['id'], *flattened)
+    
+
+    def predict(self, item_id, num):
+        """
+        Couldn't be simpler! Just retrieves the similar items and
+        their 'score' from redis.
+
+        :param item_id: string
+        :param num: number of similar items to return
+        :return: A list of lists like: [["19", 0.2203],
+        ["494", 0.1693], ...]. The first item in each sub-list is
+        the item ID and the second is the similarity score. Sorted
+        by similarity score, descending.
+        """
+
+        return self._r.zrange(self.SIMKEY % item_id,
+                              0,
+                              num-1,
+                              withscores=True,
+                              desc=True)
+
+content_engine = ContentEngine()
 
             
